4b4573649cbbcc3f75e38728eec39eb969c60534
EtzhRx <30803966+EtzhRx@users.noreply.github.com>
2018-11-10 14:24:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 
+let prefix = ", ";
+client.on("message", (message) => {
+  
+  if(message.content == 'prefix + hai') {
+      message.reply('hai juga');
+   }
+  
+  });
+      
 bot.login('NTEwNjk2ODYyNjA1NTA4NjA5.DsgHOw.Qi2dm-2PvOLJF3hD0VCXJeOjfaU');
