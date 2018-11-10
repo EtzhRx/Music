9390a13618df60b100c51dfb0b06c00eafcd30bc
EtzhRx <30803966+EtzhRx@users.noreply.github.com>
2018-11-10 14:52:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -9,15 +9,19 @@ bot.on("message", (message) => {
   if (message.content.startsWith(prefix + "aw1")) {
     message.channel.send("aw1 juga");
   } else
-   
-  if (message.content.startsWith(prefix + "aw2")) {
-    message.channel.send("aw2 juga");
-  }
+    
+  if (message.content === '/join') {
+    if (message.member.voice.channel) {
+      const connection = await message.member.voice.channel.join();
+    } else {
+      message.reply('You need to join a voice channel first!');
+    }
    
   if (message.content.startsWith(prefix + "aw3")) {
     message.channel.send("aw3 juga");
   }
 
 });
-      
+
 bot.login('NTEwNjk2ODYyNjA1NTA4NjA5.DsgHOw.Qi2dm-2PvOLJF3hD0VCXJeOjfaU');
+
