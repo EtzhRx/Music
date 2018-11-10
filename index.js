345f7604e5b5abf0d3ce73e1e8021ee2f0000f97
EtzhRx <30803966+EtzhRx@users.noreply.github.com>
2018-11-10 14:26:55 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 
-let prefix = ", ";
+let prefix = "! ";
 client.on("message", (message) => {
   
-  if(message.content == 'prefix + hai') {
-      message.reply('hai juga');
-   }
-  
-  });
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+ 
+  if (message.content.startsWith(prefix + "aw1")) {
+    message.channel.send("aw1 juga");
+  } else
+   
+  if (message.content.startsWith(prefix + "aw2")) {
+    message.channel.send("aw2 juga");
+  }
+   
+  if (message.content.startsWith(prefix + "aw3")) {
+    message.channel.send("aw3 juga");
+  }
+
+});
       
 bot.login('NTEwNjk2ODYyNjA1NTA4NjA5.DsgHOw.Qi2dm-2PvOLJF3hD0VCXJeOjfaU');
