ddb8e0b328692a9478f8aff14c5b6856c0013597
EtzhRx <30803966+EtzhRx@users.noreply.github.com>
2018-11-10 14:59:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,26 +2,23 @@ const Discord = require("discord.js");
 const bot = new Discord.Client();
 
 let prefix = "! ";
-bot.on("message", (message) => {
-  
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
- 
-  if (message.content.startsWith(prefix + "aw1")) {
-    message.channel.send("aw1 juga");
-  } else
-    
+bot.on('message', async message => {
+  // Voice only works in guilds, if the message does not come from a guild,
+  // we ignore it
+  if (!message.guild) return;
+
   if (message.content === '/join') {
+    // Only try to join the sender's voice channel if they are in one themselves
     if (message.member.voice.channel) {
       const connection = await message.member.voice.channel.join();
     } else {
       message.reply('You need to join a voice channel first!');
     }
-   
-  if (message.content.startsWith(prefix + "aw3")) {
-    message.channel.send("aw3 juga");
   }
-
 });
+  
+  
 
 bot.login('NTEwNjk2ODYyNjA1NTA4NjA5.DsgHOw.Qi2dm-2PvOLJF3hD0VCXJeOjfaU');
 
+  
